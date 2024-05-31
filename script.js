a38bbf825e094167ba90e6eb718f154f10b69b5c
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-31 11:32:12 +0530

    ADD

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	if(!s){
+		return '';
+	}
+	const initialWord = s.split(' ');
+	let firstWord = initialWord[0];
+	return firstWord;
 }
 
 // Do not change the code below
